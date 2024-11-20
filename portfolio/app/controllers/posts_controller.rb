020cb0ac3411be38858b5d52720ff2b5f10e2872
Daiki <dipiyo0120@icloud.com>
2024-11-20 12:16:22 +0900

    attached-posts-restrict

--- portfolio/app/controllers/posts_controller.rb
+++ portfolio/app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
     before_action :authenticate_user!, except: [:index, :show]
+    before_action :correct_user, only: [:edit, :destroy]
   
     def index
       @posts = Post.includes(:user, :ratings).all
@@ -54,4 +55,12 @@ class PostsController < ApplicationController
     def post_params
       params.require(:post).permit(:title, :content, :image, ratings_attributes: [:id, :value])
     end
+
+    def correct_user
+      @post = current_user.posts.find_by(id:params[:id])
+      unless @post 
+        redirect_to posts_path
+      end
+    end
+
 end
